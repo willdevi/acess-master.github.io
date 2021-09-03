b0aef7aa9de73b4a754ff3a67645442f2fd38dff
Will <williamshakespeare993@gmail.com>
2021-09-02 23:08:47 -0300

    Add files via upload

---
+++ acess-master/acess-master/acess-master/src/main/java/com/dio/acessmaster/AcessMasterApplication.java
@@ -0,0 +1,13 @@
+package com.dio.acessmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessMasterApplication.class, args);
+	}
+
+}
---
+++ acess-master/acess-master/acess-master/src/test/java/com/dio/acessmaster/AcessMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.acessmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
